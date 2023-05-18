be565f819e9c949ee1bc578893b2fad732458ec2
OctaPinball <scholtz.balint@gmail.com>
2023-05-18 19:35:08 +0200

    Fixed Camera

--- OpenGLEngine/camera.py
+++ OpenGLEngine/camera.py
@@ -9,13 +9,13 @@ SENSITIVITY = 0.04
 
 
 class Camera:
-    def __init__(self, app, position=(0, 10, 0), yaw=0, pitch=-89.99999):
+    def __init__(self, app, position=(0, 10, 0), yaw=0, pitch=-88.9):
         self.app = app
         self.aspect_ratio = app.WIN_SIZE[0] / app.WIN_SIZE[1]
         self.position = glm.vec3(position)
-        self.up = glm.vec3(0, 0, 1)
+        self.up = glm.vec3(0, 1, 0)
         self.right = glm.vec3(1, 0, 0)
-        self.forward = glm.vec3(0, -1, 0)
+        self.forward = glm.vec3(0, 0, 1)
         self.yaw = yaw
         self.pitch = pitch
         # view matrix
@@ -37,7 +37,7 @@ class Camera:
         self.forward.z = glm.sin(yaw) * glm.cos(pitch)
 
         self.forward = glm.normalize(self.forward)
-        self.right = glm.normalize(glm.cross(self.forward, glm.vec3(0, 1, 0)))
+        self.right = glm.normalize(glm.cross(self.forward, glm.vec3(1, 1, 0)))
         self.up = glm.normalize(glm.cross(self.right, self.forward))
 
     def update(self):
