20ed1a701991fa4b4c66d22679f14d517d4d90cb
OctaPinball <scholtz.balint@gmail.com>
2023-05-31 13:54:46 +0200

    Code clean

--- OpenGLEngine/vbo.py
+++ OpenGLEngine/vbo.py
@@ -7,8 +7,7 @@ from objloader import Obj
 class VBO:
     def __init__(self, ctx):
         self.vbos = {}
-        self.vbos['cube'] = CubeVBO(ctx)
-        self.vbos['cat'] = CatVBO(ctx)
+        self.vbos['obj'] = ObjVBO(ctx)
 
     def destroy(self):
         [vbo.destroy() for vbo in self.vbos.values()]
@@ -32,81 +31,13 @@ class BaseVBO:
         self.vbo.release()
 
 
-class CubeVBO(BaseVBO):
-    def __init__(self, ctx):
-        super().__init__(ctx)
-        self.format = '2f 3f 3f'
-        self.attribs = ['in_texcoord_0', 'in_normal', 'in_position']
-
-    @staticmethod
-    def get_data(vertices, indices):
-        data = [vertices[ind] for triangle in indices for ind in triangle]
-        return np.array(data, dtype='f4')
-
-    def get_vertex_data(self):
-        vertices = [(-1, -1, 1), ( 1, -1,  1), (1,  1,  1), (-1, 1,  1),
-                    (-1, 1, -1), (-1, -1, -1), (1, -1, -1), ( 1, 1, -1)]
-
-        indices = [(0, 2, 3), (0, 1, 2),
-                   (1, 7, 2), (1, 6, 7),
-                   (6, 5, 4), (4, 7, 6),
-                   (3, 4, 5), (3, 5, 0),
-                   (3, 7, 4), (3, 2, 7),
-                   (0, 6, 1), (0, 5, 6)]
-        vertex_data = self.get_data(vertices, indices)
-
-        tex_coord_vertices = [(0, 0), (1, 0), (1, 1), (0, 1)]
-        tex_coord_indices = [(0, 2, 3), (0, 1, 2),
-                             (0, 2, 3), (0, 1, 2),
-                             (0, 1, 2), (2, 3, 0),
-                             (2, 3, 0), (2, 0, 1),
-                             (0, 2, 3), (0, 1, 2),
-                             (3, 1, 2), (3, 0, 1),]
-        tex_coord_data = self.get_data(tex_coord_vertices, tex_coord_indices)
-
-        normals = [( 0, 0, 1) * 6,
-                   ( 1, 0, 0) * 6,
-                   ( 0, 0,-1) * 6,
-                   (-1, 0, 0) * 6,
-                   ( 0, 1, 0) * 6,
-                   ( 0,-1, 0) * 6,]
-        normals = np.array(normals, dtype='f4').reshape(36, 3)
-
-        vertex_data = np.hstack([normals, vertex_data])
-        vertex_data = np.hstack([tex_coord_data, vertex_data])
-        return vertex_data
-
-
-class CatVBO(BaseVBO):
+class ObjVBO(BaseVBO):
     def __init__(self, app):
         super().__init__(app)
         self.format = '2f 3f 3f'
         self.attribs = ['in_texcoord_0', 'in_normal', 'in_position']
 
     def get_vertex_data(self):
-        objs = pywavefront.Wavefront('objects/home2/cottage.obj', create_materials=True, cache=True, parse=True, collect_faces=False)
-
-        vbos = []
-        tbos = []
-        for mesh in objs.mesh_list:
-            vertices = []
-            texcoords = []
-
-            for face in mesh.vertices:
-                for vertex_index, texture_index, normal_index in face:
-                    vertex = mesh.vertices[vertex_index]
-                    texture = mesh.texcoords[texture_index]
-
-                    vertices.extend(vertex)
-                    texcoords.extend(texture)
-
-            vbo = self.ctx.buffer(vertices)
-            tbo = self.ctx.buffer(texcoords)
-
-            vbos.append(vbo)
-            tbos.append(tbo)
-        return vbos
-
         objs = pywavefront.Wavefront('objects/home2/cottage.obj', create_materials=True, cache=True, parse=True, collect_faces=False)
         obj = objs.materials.popitem()[1]
         vertex_data = []
